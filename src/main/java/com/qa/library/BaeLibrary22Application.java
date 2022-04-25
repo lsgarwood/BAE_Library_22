f2f12e883e9ac2a4773d9864307d6d9f121d628e
Lauren <ls_garwood@yahoo.co.uk>
2022-04-25 17:26:30 +0100

    LG: initial commit

---
+++ src/main/java/com/qa/library/BaeLibrary22Application.java
@@ -0,0 +1,13 @@
+package com.qa.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeLibrary22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeLibrary22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/library/BaeLibrary22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeLibrary22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
