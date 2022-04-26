d30322ac7ff287f864a7cfa2481de9b8500262c8
Lauren <ls_garwood@yahoo.co.uk>
2022-04-26 20:08:57 +0100

    LG: set up unit and integration testing files

---
+++ src/test/java/com/qa/library/controller/BookControllerIntegrationTest.java
@@ -0,0 +1,5 @@
+package com.qa.library.controller;
+
+public class BookControllerIntegrationTest {
+
+}
---
+++ src/test/java/com/qa/library/controller/BookControllerUnitTests.java
@@ -0,0 +1,8 @@
+package com.qa.library.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+public class BookControllerUnitTests {
+
+}
