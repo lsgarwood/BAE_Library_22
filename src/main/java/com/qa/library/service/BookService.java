5a5977f8ffafa2bfa7a0bb5936e0da470107c6ca
Lauren <ls_garwood@yahoo.co.uk>
2022-04-26 11:34:33 +0100

    LG: added read functions to BookService

--- src/main/java/com/qa/library/service/BookService.java
+++ src/main/java/com/qa/library/service/BookService.java
@@ -1,5 +1,7 @@
 package com.qa.library.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.qa.library.domain.Book;
@@ -21,4 +23,29 @@ public class BookService {
 		return repo.saveAndFlush(book).orElseThrow(BookNotCreatedException::new);
 	}
 
+	// get all books as list
+	public List<Book> getAll() {
+		return repo.findAll();
+	}
+
+	// get one book by Id
+	public Book getById(long id) {
+		return repo.findById(id).orElseThrow(BookNotFoundWithIdException::new);
+	}
+
+	// get one book by title
+	public Book getByTitle(String title) {
+		return repo.findByTitle(title).orElseThrow(BookNotFoundWithTitleException::new);
+
+	}
+
+	// get books by author
+	public List<Book> getByAuthor(String author) {
+		return repo.findByAuthor(author).orElseThrow(BooksNotFoundByAuthorException::new);
+	}
+
+	// get books by genre
+	public List<Book> getByGenre(String genre) {
+		return repo.findByGenre(genre).orElseThrow(BooksNotFoundByGenreException::new);
+	}
 }
